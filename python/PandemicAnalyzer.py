46907491a9ab059a46f7d677183302dffb753006
Greg Myers <greg.myers@cern.ch>
2020-04-13 12:41:15 -0400

    added yesterday's data, update plots, update fit range lower bound to use more recent bins

--- python/PandemicAnalyzer.py
+++ python/PandemicAnalyzer.py
@@ -247,7 +247,7 @@ def runAnalysis():
     logisticFunc.SetLineColor(ROOT.kMagenta)
     
     # do the fit:
-    hist.Fit(logisticFunc, '', '',6, nBins)
+    hist.Fit(logisticFunc, '', '',8, nBins)
     
     # draw the histogram and fit function together:
     c1 = ROOT.TCanvas('c1', 'c1', 800, 600)
